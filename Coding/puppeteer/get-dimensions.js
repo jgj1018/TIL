9f52221112be665bda3c2e50438b34e103a26065
jgj1018 <jgj1018@gmail.com>
2018-07-22 22:19:21 +0900

    20180722

---
+++ Coding/puppeteer/get-dimensions.js
@@ -0,0 +1,17 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://example.com');
+
+    const dimensions = await page.evaluate(() => {
+        return {
+            width: document.documentElement.clientWidth,
+            height: document.documentElement.clientHeight,
+            deviceScaleFactor: window.devicePixelRatio
+        };
+    });
+    console.log('Demensions:', dimensions);
+    await browser.close();
+})();
